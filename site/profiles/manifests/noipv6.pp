a300823ca2f2b30a729add45843ae977df023766
Cristian van Ee <cristian.vanee@cri-service.nl>
2017-04-18 11:56:44 +0200

    correct setting for diabling ipv6

--- site/profiles/manifests/noipv6.pp
+++ site/profiles/manifests/noipv6.pp
@@ -1,8 +1,12 @@
 class profiles::noipv6 {
 
   sysctl { "net.ipv6.conf.all.autoconf":
+    ensure => absent,
+  }
+
+  sysctl { "net.ipv6.conf.all.disable_ipv6":
     ensure => present,
-    value  => "0",
+    value  => "1",
     silent => true
   }
 
